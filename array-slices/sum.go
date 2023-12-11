6da7da6a77bae1145d0b11b7cbed13ca3f8fd03d
prasad-firame <pfirame18@gmail.com>
2023-12-11 22:58:47 +0530

    added code reflect.DeepEqual to test

--- array-slices/sum.go
+++ array-slices/sum.go
@@ -1,10 +1,5 @@
 package main
 
-func Sum(numbers []int) int {
-	sum := 0
-	for _, number := range numbers {
-		sum += number
-
-	}
-	return sum
+func SumAll(numbersToSum ...[]int) []int {
+	return nil
 }
--- array-slices/sum_test.go
+++ array-slices/sum_test.go
@@ -1,11 +1,14 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{4, 4}, []int{0, 4})
 	want := []int{8, 4}
-	if got != want {
-		t.Errof("got %v want %v", got, want)
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
