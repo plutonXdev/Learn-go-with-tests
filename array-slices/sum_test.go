939d774569529ad8dfc5b8c3ba1f44bf6c815c4e
prasad-firame <pfirame18@gmail.com>
2023-12-11 22:43:10 +0530

    added new sumAll test case,saw it fail

--- array-slices/sum_test.go
+++ array-slices/sum_test.go
@@ -2,26 +2,10 @@ package main
 
 import "testing"
 
-func TestSum(t *testing.T) {
-
-	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-
-		got := Sum(numbers)
-		want := 15
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
-	t.Run("collection of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-
-		got := Sum(numbers)
-		want := 6
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{4, 4}, []int{0, 4})
+	want := []int{8, 4}
+	if got != want {
+		t.Errof("got %v want %v", got, want)
+	}
 }
