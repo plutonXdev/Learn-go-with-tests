2946032273167ed31dc7a8376b79706489b3fbb4
prasad-firame <pfirame18@gmail.com>
2023-12-15 00:03:54 +0530

    interface added

--- structs/shapes.go
+++ structs/shapes.go
@@ -10,6 +10,9 @@ type Rectangle struct {
 type Circle struct {
 	Radius float64
 }
+type Shape interface {
+	Area() float64
+}
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
