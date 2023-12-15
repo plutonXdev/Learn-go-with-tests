54a87c42d8a0d0ca72138fbc51b34e5ab379f002
prasad-firame <pfirame18@gmail.com>
2023-12-15 13:49:13 +0530

    added triangle structs and methods for practice

--- structs/shapes.go
+++ structs/shapes.go
@@ -10,6 +10,11 @@ type Rectangle struct {
 type Circle struct {
 	Radius float64
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
 type Shape interface {
 	Area() float64
 }
@@ -25,4 +30,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
